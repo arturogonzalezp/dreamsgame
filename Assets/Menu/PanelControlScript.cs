52bc556ebe8e89fe95515cad6a9eaf75c1c9e56f
fernandalpez3 <ma.fernandalpez@gmail.com>
2016-11-28 23:18:14 -0600

    changes

---
+++ Assets/Menu/PanelControlScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PanelControlScript : MonoBehaviour
+{
+    public Button returnButton;
+    // Use this for initialization
+    void Start()
+    {
+        returnButton.onClick.AddListener(() => { ReturnToMenu(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 1"))
+        {
+            ReturnToMenu();
+        }
+    }
+    private void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
